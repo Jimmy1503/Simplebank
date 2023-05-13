4d18a4ea698db85c8db065ad7b8717f5e07eedf9
Jimmy Brichetto <Jimbrichetto@gmail.com>
2023-05-12 20:46:10 -0400

    added bcrypt new migrations refactored store

--- api/transfer.go
+++ api/transfer.go
@@ -10,10 +10,10 @@ import (
 )
 
 type transferRequest struct {
-	FromAccountID int64 `json:"from_account_id" binding:"required,min=1"`
-	ToAccountID int64 `json:"to_account_id" binging:"required,min=1"`
-	Amount int64 `json:"amount" binding:"required,gt=0"`
-	Currency string `json:"currency" binding:"required,currency"`
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binging:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createTransfer(ctx *gin.Context) {
@@ -33,8 +33,8 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 
 	arg := db.TransferTxParams{
 		FromAccountID: req.FromAccountID,
-		ToAccountID: req.ToAccountID,
-		Amount: req.Amount,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
 	}
 
 	result, err := server.store.TransferTX(ctx, arg)
